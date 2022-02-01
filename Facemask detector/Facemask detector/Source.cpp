e1349c361eff77fd430dcfbaf43cbe61ccab4177
Amani215 <amanibrik5@gmail.com>
2022-02-01 02:09:26 +0100

    Importing openCV

---
+++ Facemask detector/Facemask detector/Source.cpp
@@ -0,0 +1,11 @@
+#include <opencv2/imgcodecs.hpp>
+#include <opencv2/imgproc.hpp>
+#include <opencv2/highgui.hpp>
+#include <iostream>
+
+using namespace cv;
+using namespace std;
+
+int main() {
+	return 0;
+}
